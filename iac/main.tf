f838fe848fc75b7bd205b3f83b3c0e63c3fb2f7e
Mansour Kalagar <mansour@foobar.agency>
2024-05-03 23:41:33 +0200

    refactor: Remove unused AWS S3 bucket resource

--- iac/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "shortener_bucket" {
-  bucket = "shortener-bucket" # Bucket names must be globally unique # Defines who can access the bucket (e.g., private, public-read, etc.)
-
-  tags = {
-    Name        = "ShortenerBucket"
-    Environment = "Dev"
-  }
-}
